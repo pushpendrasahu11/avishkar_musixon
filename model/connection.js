30cff965b86d1841a2e52ab63321a9674eaba18a
rahul-gargcoder <96135903+rahul-gargcoder@users.noreply.github.com>
2022-11-05 20:16:33 +0530

    connection

--- model/connection.js
+++ model/connection.js
@@ -1,1 +1,14 @@
+const mongoose =require('mongoose')
+const emailval=require('email-validator')
+const userschema=require('./schema')
+mongoose.connect('mongodb+srv://Rahulgarg1:rahul94631@cluster0.z1ts96w.mongodb.net/?retryWrites=true&w=majority')
+.then(function(db){
+  console.log("db is connected");
+})
+.catch(function(err){
+  console.log(err);
+})
 
+const usermodel=mongoose.model('usermodel',userschema);
+
+module.exports=usermodel
