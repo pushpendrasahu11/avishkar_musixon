e0aed9c587be374df1da013c9f1e016784432773
rahul-gargcoder <96135903+rahul-gargcoder@users.noreply.github.com>
2022-11-05 20:17:10 +0530

    schema

---
+++ model/schema.js
@@ -0,0 +1,32 @@
+const mongoose =require('mongoose')
+
+const bcrypt=require('bcrypt');
+
+const userschema =mongoose.Schema(
+    {
+    name:{
+        type:String,
+        required:'true',
+    },
+    email:{
+        type:String,
+        required:'true',
+        unique:'true',
+        
+    },
+    password:{
+        type:String,
+        required:'true'
+    }
+});
+
+
+userschema.pre('save',async function(){
+    let salt=await bcrypt.genSalt();
+    console.log(salt);
+    let hash=await bcrypt.hash(this.password,salt);
+    this.password=hash;
+})
+module.exports=userschema;
+
+
