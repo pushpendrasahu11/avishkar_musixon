c7ff451d27e5b952799fce9ecdc14e0e42c0416e
rahul-gargcoder <96135903+rahul-gargcoder@users.noreply.github.com>
2022-11-17 13:00:49 +0530

    Update connection.js

--- model/connection.js
+++ model/connection.js
@@ -1,7 +1,7 @@
 const mongoose =require('mongoose')
 const emailval=require('email-validator')
 const userschema=require('./schema')
-mongoose.connect('mongodb+srv://Rahulgarg1:rahul94631@cluster0.z1ts96w.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://Rahulgarg1:rahul@cluster0.z1ts96w.mongodb.net/?retryWrites=true&w=majority')
 .then(function(db){
   console.log("db is connected");
   // console.log(db);
